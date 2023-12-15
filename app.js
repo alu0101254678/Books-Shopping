895689792d2aae1a65e42509cc7450cff325acde
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2023-12-15 12:45:35 +0000

    Changed content of database

--- app.js
+++ app.js
@@ -1,73 +1,34 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require ('mongoose'); // Importar el módulo de mongoose
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// Definir el modelo para la colección "ejemplo"
-const ejemploSchema = new mongoose.Schema({
-  nombre: String,
-  edad: Number,
-  // Otros campos que puedas necesitar
+// Ruta principal para verificar la conexión y realizar la conexión a la base de datos
+app.get('/', (req, res) => {
+  // Conexión a la base de datos de MongoDB Atlas
+  mongoose.connect('mongodb+srv://tu_usuario:tu_contraseña@cluster0.ffwce0q.mongodb.net/tu_basededatos', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log('Conexión exitosa a MongoDB Atlas');
+      res.send('Conexión exitosa a la base de datos');
+    })
+    .catch((error) => {
+      console.error('Error de conexión a MongoDB:', error);
+      res.status(500).send('Error interno del servidor');
+    });
 });
 
-const Ejemplo = mongoose.model('Ejemplo', ejemploSchema);
-
-// Conexión a la base de datos de MongoDB Atlas
-mongoose.connect('mongodb+srv://alu0101254678:Fin_del_Creador_2022@cluster0.ffwce0q.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// Manejo de errores 404
+app.use(function (req, res, next) {
+  res.status(404).send('Recurso no encontrado');
 });
 
-// Verifica la conexión a la base de datos
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
-db.once('open', function () {
-  console.log('Conexión exitosa a MongoDB Atlas');
-});
-
-// Pasar el modelo a las rutas
-app.use((req, res, next) => {
-  req.db = {
-    Ejemplo: Ejemplo, // Asegúrate de que Ejemplo esté disponible en req.db
-  };
-  next();
-});
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Manejo de errores generales
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Error interno del servidor');
 });
 
 module.exports = app;
