42b1e8735731f3e5202541cfbbc5d35ea52ef7fd
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2023-12-15 11:58:00 +0000

    Added conection to MongoDB Atlas

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose'); // Agrega la importación de mongoose
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +23,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Conexión a la base de datos de MongoDB Atlas
+mongoose.connect('mongodb+srv://alu0101254678:Fin_del_Creador_2022@cluster0.ffwce0q.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Verifica la conexión a la base de datos
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.once('open', function () {
+  console.log('Conexión exitosa a MongoDB Atlas');
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
