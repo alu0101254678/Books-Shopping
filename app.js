5e3982875e76c28eecd1918393b0bce2a9870324
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2023-12-15 13:05:42 +0000

    Changed content of database

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ const mongoose = require('mongoose');
 
 var app = express();
 
+// Conexión a la base de datos de MongoDB Atlas
+mongoose.connect('mongodb+srv://alu0101254678:Fin_del_Creador_2022@cluster0.ffwce0q.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Conexión exitosa a MongoDB Atlas');
+    res.send('Conexión exitosa a la base de datos');
+  })
+  .catch((error) => {
+    console.error('Error de conexión a MongoDB:', error);
+    res.status(500).send('Error interno del servidor');
+  });
 
 // Ruta principal para verificar la conexión y realizar la conexión a la base de datos
 app.get('/', (req, res) => {
-  // Conexión a la base de datos de MongoDB Atlas
-  mongoose.connect('mongodb+srv://alu0101254678:Fin_del_Creador_2022@cluster0.ffwce0q.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => {
-      console.log('Conexión exitosa a MongoDB Atlas');
-      res.send('Conexión exitosa a la base de datos');
-    })
-    .catch((error) => {
-      console.error('Error de conexión a MongoDB:', error);
-      res.status(500).send('Error interno del servidor');
-    });
+  res.send('Bienvenido a la API de Books Shopping');
 });
 
 // Manejo de errores 404
