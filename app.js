4feb49bfd088d1d08e5662d67be71f35dc3f317d
Yago Pérez Molanes <72870696+alu0101254678@users.noreply.github.com>
2023-12-23 09:20:14 +0000

    Added correct connectivity to Azure Cosmos DB

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
 
-const options = {
-  key: fs.readFileSync(path.join(__dirname, 'privatekey.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'certificate.pem'))
-};
+const app = express();
+const PORT = process.env.PORT || 3000;
+let messagge = "";
 
-var mongoClient = require("mongodb").MongoClient;
-mongoClient.connect("mongodb://alu0101254678:Ivlc6PhBZ9i71tj7Q5Jx8b39ZV4b4yccm2Hgwste6YYGcLl3hUSv5YxrNrAiEoUT8UG5fGd2OFeVACDbdeOomg%3D%3D@alu0101254678.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@alu0101254678@", function (err, client) {
-  if (err) {
-    console.error("Error al conectar con la base de datos:", err);
-  } else {
-    console.log("Conexión exitosa a la base de datos", client.PORT);
-    // Realiza aquí cualquier operación relacionada con la base de datos que desees loguear.
-  }
+// Conexión a la base de datos de MongoDB en Azure Cosmos DB
+const mongoURI = "mongodb://alu0101254678:Ivlc6PhBZ9i71tj7Q5Jx8b39ZV4b4yccm2Hgwste6YYGcLl3hUSv5YxrNrAiEoUT8UG5fGd2OFeVACDbdeOomg%3D%3D@alu0101254678.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@alu0101254678@";
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Conexión exitosa a Azure Cosmos DB');
+    messagge = 'Conexión exitosa a Azure Cosmos DB';
+  })
+  .catch((error) => {
+    console.error('Error de conexión a Azure Cosmos DB:', error);
+  });
+
+// Ruta principal para verificar la conexión y realizar la conexión a la base de datos
+app.get('/', (req, res) => {
+  res.send(messagge);
 });
 
-const server = https.createServer(options, (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hola, mundo seguro con SSL!\n');
+// Manejo de errores 404
+app.use((req, res, next) => {
+  res.status(404).send('Recurso no encontrado');
 });
 
-const PORT = 3000;
-
-server.listen(PORT, () => {
-  console.log(`Servidor Node.js con SSL escuchando en el puerto ${PORT}`);
+// Manejo de errores generales
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Error interno del servidor');
 });
 
+app.listen(PORT, () => {
+  console.log(`Servidor en ejecución en el puerto ${PORT}`);
+});
